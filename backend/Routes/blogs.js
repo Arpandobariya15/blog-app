146014b3ae868b9abe9d097dfc175a2ac1fcfe99
Arpandobariya15 <work.arpan8980@gmail.com>
2024-12-20 10:01:15 +0530

    routesIsComplet

--- backend/Routes/blogs.js
+++ backend/Routes/blogs.js
@@ -11,5 +11,45 @@ router.post("/post", async (req,res)=>{
         res.status(400).json({message:"some error grt from post route"})
     }
 })
+// get all blogs
+router.get("/getAll", async (req,res)=>{
+    try {
+        const data = await blogs.find().sort({createdAt : -1});
+        res.status(200).json({data : data})
+    } catch (error) {
+        res.status(400).json({message:"some error grt from get route"})
+    }
+})
+// get recent 3 blog
+router.get("/getRecentblogs", async (req,res)=>{
+    try {
+        const data = await blogs.find().sort({createdAt : -1}).limit(3);
+        res.status(200).json({data : data})
+    } catch (error) {
+        res.status(400).json({message:"some error grt from get route"})
+    }
+})
+// get blogs by id
+router.get("/getBlog/:id", async (req,res)=>{
+    try {
+        const {id}= req.params;
+        const data = await blogs.findById(id);
+        res.status(200).json({data : data})
+    } catch (error) {
+        res.status(400).json({message:"some error grt from get route"})
+    }
+})
+// update blogs daata
+router.put("/updateBlog/:id", async (req,res)=>{
+    try {
+        const {id}= req.params;
+        const { title,desc }= req.body;
+        await blogs.findByIdAndUpdate(id, { title,desc });
+        res.status(200).json({message : "Data was updated by update route"})
+    } catch (error) {
+        res.status(400).json({message:"some error grt from get route"})
+    }
+})
+
 
 module.exports = router;
